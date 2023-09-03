0dc20d68bfb92d8d4ee89d49d5399c681e200af7
Alexei <slgaming@mail.ru>
2023-09-04 01:23:37 +0300

    fix forum button

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("forum/", views.forum_view, name="forum"),
+    path("forum", views.forum_view, name="forum"),
     path("forum/<int:question_id>", views.question_view, name="question"),
     path("forum/<int:question_id>/remove_question", views.remove_question, name="remove_question"),
     path("forum/<int:question_id>/answer_question", views.answer_question, name="answer_question"),
