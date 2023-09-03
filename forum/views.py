c119d7de2f4db97e00721f72b38e5e774b338525
Alexei <slgaming@mail.ru>
2023-09-04 00:32:05 +0300

    #3

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -12,6 +13,7 @@ def index(request):
     return render(request, 'forum/index.html')
 
 
+@login_required()
 def forum_view(request):
     if query := request.GET.get('q'):
         questions = Question.objects.filter(Q(title__icontains=query) | Q(text__icontains=query))
@@ -25,6 +27,7 @@ def forum_view(request):
     return render(request, 'forum/forum.html', context=context)
 
 
+@login_required()
 def question_view(request, question_id):
     question = get_object_or_404(Question, id=question_id)
     comments = Comment.objects.filter(question=question)
@@ -35,6 +38,7 @@ def question_view(request, question_id):
     return render(request, 'forum/question_page.html', context=context)
 
 
+@login_required()
 def create_question(request):
     if request.method == "POST":
         form = QuestionForm(data=request.POST)
@@ -43,11 +47,13 @@ def create_question(request):
             return redirect("question", question_id=obj.id)
 
 
+@login_required()
 def remove_question(request, question_id):
     Question.objects.filter(id=question_id).delete()
     return redirect("forum")
 
 
+@login_required()
 def answer_question(request, question_id):
     qst = Question.objects.get(id=question_id)
     qst.is_closed = True
@@ -55,6 +61,7 @@ def answer_question(request, question_id):
     return redirect("question", question_id=qst.id)
 
 
+@login_required()
 def create_comment(request, question_id):
     if request.method == "POST":
         question = Question.objects.get(id=question_id)
@@ -89,7 +96,26 @@ def login_request(request):
     return render(request=request, template_name="forum/auth/login.html", context={"login_form": form})
 
 
+@login_required()
 def logout_request(request):
     logout(request)
     messages.info(request, "You have successfully logged out.")
     return redirect("index")
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, "Password changed.")
+            return redirect("index")
+        print(form.errors)
+    else:
+        form = PasswordChangeForm(request.user)
+    data = {
+        'form': form
+    }
+    return render(request, "forum/auth/pass_change.html", data)
